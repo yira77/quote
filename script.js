ff6306cc766775c74db69427f93bbc021f5da464
Yira <159470852+yira77@users.noreply.github.com>
2024-02-15 22:54:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const quoteText = document.querySelector(".quote"),
- author = document.querySelector(".author .name"),
-    quoteBtn = document.querySelector("button");
-soundBtn = document.querySelector(".sound"),
-    copyBtn = document.querySelector(".copy"),
-    twitterBtn = document.querySelector(".twitter"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter"),
+synth = speechSynthesis;
 
-
-    soundBtn.addEventListener("click", () => {
-        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by${author.innerText}`);
-        speechSynthesis.speak(utterance);
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "New Quote";
     });
+}
 
-copyBtn.addEventListener("click", () => {
+speechBtn.addEventListener("click", ()=>{
+    if(!quoteBtn.classList.contains("loading")){
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(()=>{
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
+});
+
+copyBtn.addEventListener("click", ()=>{
     navigator.clipboard.writeText(quoteText.innerText);
 });
 
-twitterBtn.addEventListener("click", () => {
+twitterBtn.addEventListener("click", ()=>{
     let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
     window.open(tweetUrl, "_blank");
 });
 
-quoteBtn.addEventListener("click", () => {
-    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
-        quoteText.innerText = result.content;
-        author.innerText = result.author;
-    });
-});
+quoteBtn.addEventListener("click", randomQuote);
